387f231525db5c3bf4e3e8e665233263f23a8174
tonyr346 <114841952+tonyr346@users.noreply.github.com>
2023-10-05 21:05:39 +0000

    105

--- Programs/HEllow/Program.cs
+++ Programs/HEllow/Program.cs
@@ -22,12 +22,49 @@ class Program
 
         double[] numbers1 = {0,32, 4,6} ;
 
-        foreach (double number in numbers1)
+        ///foreach (double number in numbers1)
+        /// {
+        ///    Console.WriteLine($"{number}");
+        /// }
+
+        List<int> Values = new List<int>();
+
+        Values.Add(5);
+        Values.Add(7);
+        Values.Add(a);
+        Values.Add(b);
+        Values.Add(c);
+        Values.Add(9);
+
+        int [] numbers2 = new int [Values.Count];
+        int [] numbers3 = new int [Values.Count];
+        
+        Console.WriteLine($"Length of List = {Values.Count}");
+
+        int tot1=0;
+
+        for (int i=0; i<Values.Count; i++)
         {
-            Console.WriteLine($"{number}");
+            tot1 = tot1+Values[i];
+            numbers2[i] = tot1;
+            numbers3[i] = i;
         }
 
+        var res = numbers2.Zip(numbers3,(n,w) => new {Number = n, Word = w});
+        
+        Console.WriteLine($"Total Value of List = {tot1}");
 
-        Console.WriteLine(d);
+    
+        foreach(int numbers in numbers2)
+        {
+           
+            Console.WriteLine(numbers);
+         }
+
+        foreach(var z in res)
+        {
+            Console.WriteLine($"Index {z.Word} = {z.Number}");
+        }
+
     }
 }
