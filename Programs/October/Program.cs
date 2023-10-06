cb140f618886af641cc43a35a7ff0e944103f0a0
tonyr346 <114841952+tonyr346@users.noreply.github.com>
2023-10-06 15:51:42 +0000

    106b

--- Programs/October/Program.cs
+++ Programs/October/Program.cs
@@ -10,18 +10,26 @@ class Program
     
     static void Main(string[] args)
     {
+        int bal = 1000;
+       
         Console.Write("Enter a Number: ");
         int a = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine($"You Entered: {a}");
 
         Console.Write("Enter a Number: ");
         int b = Convert.ToInt32(Console.ReadLine());
 
         int c = Calc1(a,b);
-        
-        Console.WriteLine($"Solution: {c}");
 
+        bal = bal-c;
 
+        Console.WriteLine($"Balence: {bal}");
+        int counter = 0;
+        for (int i=0; i<20; i++)
+        {
+            counter = counter+1;
+            bal = bal-i;
+        }
+        Console.WriteLine($"Number of Transactions: {counter}");
+        Console.WriteLine($"Balence: {bal}");
     }
 }
