4403acb3a69ffb0f7d6173385b608522688af4da
tonyr346 <114841952+tonyr346@users.noreply.github.com>
2023-10-06 15:27:44 +0000

    106a

--- Programs/October/Program.cs
+++ Programs/October/Program.cs
@@ -2,8 +2,26 @@
 
 class Program
 {
+    static int Calc1 (int a, int b)
+    {
+        int c = a*b;
+        return c;
+    }
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.Write("Enter a Number: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"You Entered: {a}");
+
+        Console.Write("Enter a Number: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        int c = Calc1(a,b);
+        
+        Console.WriteLine($"Solution: {c}");
+
+
     }
 }
