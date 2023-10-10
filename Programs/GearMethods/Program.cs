ffa1894a736635d33a512d2b70b9b8b30a0a9be0
tonyr346 <114841952+tonyr346@users.noreply.github.com>
2023-10-10 15:50:34 +0000

    1010a

--- Programs/GearMethods/Program.cs
+++ Programs/GearMethods/Program.cs
@@ -1,4 +1,23 @@
 ﻿namespace GearMethods;
+ 
+public class GSBasic
+{
+    string ID;
+    public int Np, Ng;
+    public double ratio;
+    public double GetRatio ()
+    {
+        ratio = (double) Ng/Np;
+        return ratio;
+    }
+    
+  
+
+
+    public GSBasic (string iD,int np, int ng)
+    {ID = iD; Np=np; Ng=ng; Ratio = Math.Round(GetRatio(),4);}
+
+}
 
 class Program
 {
@@ -7,6 +26,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("-----------Gear Program Starting-----------");
-        
+
     }
 }
