98d859ed358fccdace2bf296b29e7c13420117aa
tonyr346 <114841952+tonyr346@users.noreply.github.com>
2023-10-10 15:51:20 +0000

    1010b

--- Programs/GearMethods/Program.cs
+++ Programs/GearMethods/Program.cs
@@ -10,10 +10,6 @@ public class GSBasic
         ratio = (double) Ng/Np;
         return ratio;
     }
-    
-  
-
-
     public GSBasic (string iD,int np, int ng)
     {ID = iD; Np=np; Ng=ng; Ratio = Math.Round(GetRatio(),4);}
 
