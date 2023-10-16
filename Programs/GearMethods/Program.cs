1baa5db0c1725c21ec809e4a0100c4669879b50c
tonyr346 <114841952+tonyr346@users.noreply.github.com>
2023-10-16 17:52:33 +0000

    1016

--- Programs/GearMethods/Program.cs
+++ Programs/GearMethods/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace GearMethods;
+﻿using System.Transactions;
+
+namespace GearMethods;
  
 public class GSBasic
 {
@@ -10,18 +12,30 @@ public class GSBasic
         ratio = (double) Ng/Np;
         return ratio;
     }
+
+    
     public GSBasic (string iD,int np, int ng)
-    {ID = iD; Np=np; Ng=ng; Ratio = Math.Round(GetRatio(),4);}
+    {ID = iD; Np=np; Ng=ng; ratio = Math.Round(GetRatio(),4);}
 
 }
 
 class Program
 {
-    
+   
     
     static void Main(string[] args)
     {
+        GSBasic HS = new GSBasic("010",19,64);
+        GSBasic INTER = new GSBasic("010",17,82);
+        GSBasic SS = new GSBasic("010",19,64);
+
+        
+        
         Console.WriteLine("-----------Gear Program Starting-----------");
+        Console.WriteLine(HS.Np);
+        double totalRatio = HS.ratio*INTER.ratio*SS.ratio;
+        Console.WriteLine(Convert.ToString(Math.Round(totalRatio,4)));
+        
 
     }
 }
