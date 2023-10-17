bf87b23e784eb08fa39abbe417152f78fb73e046
tonyr346 <114841952+tonyr346@users.noreply.github.com>
2023-10-17 20:37:51 +0000

    1017

--- Programs/GearMethods/Program.cs
+++ Programs/GearMethods/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Transactions;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Transactions;
 
 namespace GearMethods;
  
@@ -22,20 +23,86 @@ public class GSBasic
 class Program
 {
    
+    static double InvRad(double a)
+    {
+        double alpha = Math.Tan(a)-a;
+        return alpha;
+    }
+
+    static double Conv2Deg(double a)
+    {
+        double b = a*180/Math.PI;
+        return b; 
+    }
+
+    static double fPrime(double x)
+    {
+        double xPrime = 1/(Math.Pow(Math.Cos(x),2))+1;
+        return xPrime;
+    }
+
+    static double fx (double x,double inva)
+    {
+        double fxVal = inva- Math.Tan(x)+x;
+        return fxVal; 
+    }
+
+    static double NewtonMethod(double x0, double tolerance, int MaxIterations, double inva)
+    {
+        double x = x0;
+        int iterations = 0;
+
+        while (iterations<MaxIterations)
+        {
+            double fxc = fx(x, inva);
+            double fxd = fPrime(x);
+
+            double deltaX = x-fxc/fxd;
+
+            if(Math.Abs(deltaX)<tolerance)
+            {
+                return x;
+            }
+            
+            x -= deltaX+x;
+
+            iterations++;
+        }
+
+        return double.NaN; 
+    }
+
+
+    static double Conv2Rad(double a)
+    {
+        double b = a*Math.PI/180;
+        return b;
+    }
+
     
     static void Main(string[] args)
     {
-        GSBasic HS = new GSBasic("010",19,64);
-        GSBasic INTER = new GSBasic("010",17,82);
-        GSBasic SS = new GSBasic("010",19,64);
-
-        
+     
         
         Console.WriteLine("-----------Gear Program Starting-----------");
-        Console.WriteLine(HS.Np);
-        double totalRatio = HS.ratio*INTER.ratio*SS.ratio;
-        Console.WriteLine(Convert.ToString(Math.Round(totalRatio,4)));
         
+        double x0 = .35;
+        double tolerance = 0.0000001;
+        int maxIterations = 1000000000;
+        double inva = 0.043;
+
+        double x = NewtonMethod(x0,tolerance, maxIterations, inva);
+
+        if (double.IsNaN(x))
+        {
+            Console.WriteLine("Failed to Converge");
+        }
+        else
+        {
+            double angle = Conv2Deg(x);
+            Console.WriteLine("Value:" + angle);
+        }
+    
 
     }
 }
