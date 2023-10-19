8a102fa50b1edc06d91eb584f9b44f3f8e988eed
tonyr346 <114841952+tonyr346@users.noreply.github.com>
2023-10-19 14:19:01 +0000

    1019a

--- Programs/TROCT/Program.cs
+++ Programs/TROCT/Program.cs
@@ -2,18 +2,37 @@
 
 class Program
 {
+    
+    static void StartMessage()
+    {
+        Console.WriteLine("---1, ADD DATA----");
+        Console.WriteLine("---2, COMPUTE-----");
+        Console.WriteLine("---3, COMPUTE-----");
+        Console.WriteLine("---9, EXIT--------");
+        return ;
+    }
+
+    static string GetInput(string message)
+    {
+        Console.Write(message);
+        string a = Console.ReadLine()!;
+        return a;
+    }
+    
     static void Main(string[] args)
     {
         Console.WriteLine("-----------PROGRAM STARTING-----------");
 
-        Console.WriteLine("---1, ADD DATA----");
-        Console.WriteLine("---2, COMPUTE-----");
-
-        string a = Console.ReadLine()!;
-        int c = Convert.ToInt32(a);
         int b;
+                
+        StartMessage();
 
-        switch (c)
+        int e = Convert.ToInt32(GetInput("Enter a Number: "));
+
+     
+           
+
+        switch (e)
         {
 
             case 1:
@@ -22,9 +41,18 @@ class Program
 
             case 2: 
             Console.WriteLine("You Chose Compute");
-            b = c*5;
+            b = e*5;
             Console.WriteLine($"A x 5 = {b}");
             break;
+
+            case 3:
+            for (int i = 0; i<10; i++)
+            {
+                int counter = i+1;
+                Console.WriteLine(counter);
+            }
+            break;
+
 
             default:
             Console.WriteLine("Invalid Input");
