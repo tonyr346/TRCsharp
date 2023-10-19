729de308ffb4c4c6894cfe509a6fe3c694e91a6c
tonyr346 <114841952+tonyr346@users.noreply.github.com>
2023-10-19 12:32:37 +0000

    1019

--- Programs/GearMethods/Program.cs
+++ Programs/GearMethods/Program.cs
@@ -1,108 +1,81 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Transactions;
+﻿using System;
+using System.Security.AccessControl;
 
-namespace GearMethods;
- 
-public class GSBasic
+namespace GearMethods
 {
-    string ID;
-    public int Np, Ng;
-    public double ratio;
-    public double GetRatio ()
+    public class GSBasic
     {
-        ratio = (double) Ng/Np;
-        return ratio;
-    }
+        private string ID;
+        public int Np, Ng;
+        public double Ratio { get; private set; }
 
-    
-    public GSBasic (string iD,int np, int ng)
-    {ID = iD; Np=np; Ng=ng; ratio = Math.Round(GetRatio(),4);}
-
-}
-
-class Program
-{
-   
-    static double InvRad(double a)
-    {
-        double alpha = Math.Tan(a)-a;
-        return alpha;
-    }
-
-    static double Conv2Deg(double a)
-    {
-        double b = a*180/Math.PI;
-        return b; 
-    }
-
-    static double fPrime(double x)
-    {
-        double xPrime = 1/(Math.Pow(Math.Cos(x),2))+1;
-        return xPrime;
-    }
-
-    static double fx (double x,double inva)
-    {
-        double fxVal = inva- Math.Tan(x)+x;
-        return fxVal; 
-    }
-
-    static double NewtonMethod(double x0, double tolerance, int MaxIterations, double inva)
-    {
-        double x = x0;
-        int iterations = 0;
-
-        while (iterations<MaxIterations)
+        public GSBasic(string id, int np, int ng)
         {
-            double fxc = fx(x, inva);
-            double fxd = fPrime(x);
+            ID = id;
+            Np = np;
+            Ng = ng;
+            CalculateRatio();
+        }
 
-            double deltaX = x-fxc/fxd;
+        private void CalculateRatio()
+        {
+            Ratio = (double)Ng / Np;
+        }
+    }
 
-            if(Math.Abs(deltaX)<tolerance)
+    public static class AngleConversion
+    {
+        public static double InvRad(double a) => Math.Tan(a) - a;
+        public static double Conv2Deg(double a) => a * 180 / Math.PI;
+        public static double Conv2Rad(double a) => a * Math.PI / 180;
+    }
+
+    public static class NewtonMethod
+    {
+        public static double CalculateRoot(double x0, double tolerance, int maxIterations, double inva)
+        {
+            double x = x0;
+
+            for (int iterations = 0; iterations < maxIterations; iterations++)
             {
-                return x;
+                double fxc = inva - Math.Tan(x) + x;
+                double fxd = -1+(1/(Math.Pow(Math.Cos(x),2)));
+
+                double deltaX = x - fxc / fxd;
+
+                if (Math.Abs(deltaX) < tolerance)
+                {
+                    return x;
+                }
+
+                x = deltaX;
             }
-            
-            x -= deltaX+x;
 
-            iterations++;
+            throw new InvalidOperationException("Failed to converge");
         }
-
-        return double.NaN; 
     }
 
-
-    static double Conv2Rad(double a)
+    class Program
     {
-        double b = a*Math.PI/180;
-        return b;
-    }
-
-    
-    static void Main(string[] args)
-    {
-     
-        
-        Console.WriteLine("-----------Gear Program Starting-----------");
-        
-        double x0 = .35;
-        double tolerance = 0.0000001;
-        int maxIterations = 1000000000;
-        double inva = 0.043;
-
-        double x = NewtonMethod(x0,tolerance, maxIterations, inva);
-
-        if (double.IsNaN(x))
+        static void Main(string[] args)
         {
-            Console.WriteLine("Failed to Converge");
-        }
-        else
-        {
-            double angle = Conv2Deg(x);
-            Console.WriteLine("Value:" + angle);
-        }
-    
+            Console.WriteLine("-----------Gear Program Starting-----------");
 
+            double x0 = 0.15;
+            double tolerance = 0.0001;
+            int maxIterations = 1000000000;
+            double inva = 0.019;
+
+            try
+            {
+                double x = NewtonMethod.CalculateRoot(x0, tolerance, maxIterations, inva);
+                double angle = AngleConversion.Conv2Deg(x);
+                Console.WriteLine("Value: " + angle);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
---
+++ Programs/TROCT/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace TROCT;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("-----------PROGRAM STARTING-----------");
+
+        Console.WriteLine("---1, ADD DATA----");
+        Console.WriteLine("---2, COMPUTE-----");
+
+        string a = Console.ReadLine()!;
+        int c = Convert.ToInt32(a);
+        int b;
+
+        switch (c)
+        {
+
+            case 1:
+            Console.WriteLine("You Chose Add");
+            break;
+
+            case 2: 
+            Console.WriteLine("You Chose Compute");
+            b = c*5;
+            Console.WriteLine($"A x 5 = {b}");
+            break;
+
+            default:
+            Console.WriteLine("Invalid Input");
+            break;
+
+
+        }
+
+        
+
+
+
+        
+        Console.WriteLine("-----------PROGRAM END-----------");
+    }
+}
