48633173006db61d786a2ef355cea4389ae9cff4
tonyr346 <114841952+tonyr346@users.noreply.github.com>
2023-10-25 20:02:39 +0000

    1025

---
+++ Programs/GearOCT/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace GearOCT;
+
+public class GearSet ()
+{
+    
+    
+}
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int Np = 21;
+        int Ng = 66;
+        double Pn = 2.5000;
+        double PAn = 25.000;
+        double HA = 13.000;
+        string HAdir = "LEFT";
+        double PD = 2.1313;
+        double OD = 2.3870;
+        int Q = 11;
+        double Add = 0.1329;
+        double WD = 0.2350;
+        double BLmin = 0.007;
+        double BLmax = 0.011;
+        double X1 = 0.3500;
+        double a = 1.0000;
+        double ChAdd = 0.1366;
+        double TTmax = 0.1831;
+        double TTmin = 0.1803;
+        int Nspan = 4;
+        double SpanMax = 1.0672;
+        double SpanMin = 1.0646;
+        string Mate = "712501-203";
+        double CD = 4.4095;
+        double ODmate = 6.8259;
+
+        
+
+
+    }
+}
