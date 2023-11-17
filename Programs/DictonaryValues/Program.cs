2a81d4d8b1eef4b8d4bd378199eeb1db5a856219
tonyr346 <114841952+tonyr346@users.noreply.github.com>
2023-11-17 15:03:41 +0000

    1117

---
+++ Programs/DictonaryValues/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace DictonaryValues;
+
+
+class Program
+{
+    
+    public class Messages
+    {
+        public string M1 = "Hello";
+        public string M2 = "Program Complete";
+        public string UsrIn = "Enter a value: ";
+
+        public string lne = "----------------------------------";
+
+      
+    }
+
+    static void Display(string a)
+    {
+        Console.WriteLine(a);
+        return;
+    }
+
+    static string GetData(string Message)
+    {
+        string usrstr = "";
+        Console.WriteLine(Message);
+        usrstr = Console.ReadLine()!;
+        return usrstr;
+    }
+    public static bool check(string s) 
+    { 
+        return (s == null || s == String.Empty) ? true : false; 
+    } 
+
+    public static bool IsDouble(string text)
+    {
+         Double num = 0;
+         bool isDouble = false;
+ 
+        // Check for empty string.
+        if (check(text))
+        {
+            return false;
+        }
+ 
+        isDouble = Double.TryParse(text, out num);
+ 
+            return isDouble;
+        }
+
+
+
+
+    static void Main(string[] args)
+     {
+        Messages Dialog = new Messages(); 
+        
+        
+        Display(Dialog.lne);
+        Display(Dialog.M2);
+        Display(Dialog.lne);
+    }
+}
