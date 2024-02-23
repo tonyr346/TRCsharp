6d09443326d9a3771d671daeef36016beb4140ab
tonyr346 <114841952+tonyr346@users.noreply.github.com>
2024-02-23 21:07:57 +0000

    104

--- Programs/C_Hello/Program.cs
+++ Programs/C_Hello/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Is thsi cORRECT");
+        Console.WriteLine("THIS IS CORRECT");
+
+        double[] arrayone  = new double[15];
+        int len = arrayone.Length;
+
+        for (int i=0; i<len; i++ )
+        {
+            arrayone[i] = i*0.8714;
+            Console.WriteLine(arrayone[i]);
+        }
+
     }
 }
