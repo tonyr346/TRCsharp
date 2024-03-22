c6b7f8f3e9b473ecf4924109a77592eb8ed780ce
tonyr346 <114841952+tonyr346@users.noreply.github.com>
2024-03-22 18:55:55 +0000

    10950

--- Programs/TRMAR/STREAMER/Program.cs
+++ Programs/TRMAR/STREAMER/Program.cs
@@ -1,5 +1,8 @@
 ﻿namespace STREAMER;
-
+using System.IO;
+using Newtonsoft.Json;
+using CsvHelper;
+using CsvHelper.Configuration;
 
 
 class Program
@@ -21,24 +24,26 @@ class Program
     
     static void Main(string[] args)
     {
-        string outputfolder = "/workspaces/TRCsharp/Programs/TRMAR/STREAMER";
+        string outputfolder = "/workspaces/TRCsharp/Programs/TRMAR/STREAMER/outputs";
         string defaultstr = "default.csv";
-        string defaultOut = outputfolder + defaultstr;
+        string defaultOut = Path.Combine(outputfolder, defaultstr);
         string name = "Position";
-        
+        double j = 2;
 
         dataRow val1 = new dataRow(name, 1.00, "IN");
 
         List<dataRow> Position = new List<dataRow>();
 
-        for (int i=1; i<10; i++)
-        {
+        for (double i=2; i<500;i++)
+            {
             
             string nm = name + Convert.ToString(i);
-            double p = 2*i+Math.Pow(i,2)/Math.Pow(i,3);
+            double p = 2*j+Math.Pow(j,2)/Math.Pow(j,3)-Math.Pow(j,2);
 
-            dataRow nm1 = new dataRow(Convert.ToString(i+1), p , "in" );
+            dataRow nm1 = new dataRow(Convert.ToString(i), p , "in" );
             Position.Add(nm1);
+            j = j+.002;
+            
         }        
 
 
@@ -47,11 +52,21 @@ class Program
         {
             
             string rounded =Math.Round(row.VAL, 3).ToString("F3");
-            string paddedb = rounded.PadLeft(8,'0');
+            string paddedb = rounded;
             Console.WriteLine(paddedb);
         }
         
-              
+
+          using (StreamWriter writer = new StreamWriter(defaultOut))
+            {
+                foreach (dataRow row in Position)
+                {
+                    string roundedValue = Math.Round(row.VAL, 3).ToString("F5"); // Round to 3 decimal places
+                    string paddedValue = roundedValue;
+                    string line = string.Join(",",row.N, paddedValue, row.U);
+                    writer.WriteLine(line);
+                }
+            } 
         
        
         
