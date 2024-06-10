f28b9e19b5a70873a2e3068770a42074cfcbc5b2
tonyr346 <114841952+tonyr346@users.noreply.github.com>
2024-06-10 20:50:26 +0000

    150650

---
+++ Programs/TRJUN24/AMORT/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace AMORT;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter Years:  ");
+        int A = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Years Entered: {A}");
+    }
+}
