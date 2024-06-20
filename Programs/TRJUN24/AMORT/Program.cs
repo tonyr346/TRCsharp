2025dbfe9c95c58f9b7902b29c8c81118e97b0ef
tonyr346 <114841952+tonyr346@users.noreply.github.com>
2024-06-20 20:55:14 +0000

    10893280

--- Programs/TRJUN24/AMORT/Program.cs
+++ Programs/TRJUN24/AMORT/Program.cs
@@ -4,9 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Enter Years:  ");
+        
+        Console.Write("Enter Principle:     ");
+        int P = Convert.ToInt32(Console.ReadLine());
+        
+        
+        Console.Write("Enter Years:         ");
         int A = Convert.ToInt32(Console.ReadLine());
 
-        Console.WriteLine($"Years Entered: {A}");
+        Console.Write("Enter Rate (%APR):   ");
+        double b =  Convert.ToDouble(Console.ReadLine());
+
+        double r = (double)b/100/12;
+        int n = A*12;
+      
+        
+        double Payment = P*((r*Math.Pow((1+r),n))/((Math.Pow((1+r),n)-1)));
+
+        string PMT = "$ " + Convert.ToString(Math.Round(Payment, 2));
+
+        Console.WriteLine("Payment:           " + PMT);
+
+
     }
 }
