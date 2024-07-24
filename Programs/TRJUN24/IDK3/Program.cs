b74218a5f40c0131a22de57a4e5d8ee019ba65be
tonyr346 <114841952+tonyr346@users.noreply.github.com>
2024-07-24 20:47:01 +0000

    151981

---
+++ Programs/TRJUN24/IDK3/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace IDK3;
+
+class Program
+{
+    
+    public class IOBoard
+    {
+        public int p1=0;
+        public int p2=0;
+        public int p3=0;
+        public int p4=0;
+        public int p5=1;
+        public int p6=0;
+        public int p7=0;
+        public int p8=0;
+
+        public int[] state;
+        
+        public IOBoard()
+        {
+            state =new int [] {p1,p2,p3,p4,p5,p6,p7,p8};
+        
+        }
+        
+    }
+    
+    
+    static void Main(string[] args)
+    {
+         IOBoard ioBoard1 = new IOBoard();
+
+            for (int i = 0; i < ioBoard1.state.Length; i++)
+            {
+                ioBoard1.state[i] = 0;
+            }
+
+            // Output the state array to verify changes
+            foreach (var i in ioBoard1.state)
+            {
+                Console.WriteLine(i);
+            }
+
+            
+
+    }
+}
